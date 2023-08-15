d56a350c41282813a3a9a6e7c713ae6ef9e14362
Bugendai1234 <142224558+Bugendai1234@users.noreply.github.com>
2023-08-15 19:02:39 +0000

    Added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 # create object of the class
 app = Flask(__name__)
 #create route (URL after domain name)
 @app.route("/")
 def hello_world():
-  return "Hello, everyone"
+  return render_template('home.html')
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
